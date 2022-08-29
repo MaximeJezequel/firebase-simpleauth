1fc5a768407d8fe17ed7651dd46d62b3773f6f0e
Maxime Jezequel <maxime.jezequel@gmail.com>
2022-08-29 23:26:15 +0200

    Create a Home component + change default route to login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 
 import Account from "./components/Account"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
 import ProtectedRoute from "./components/ProtectedRoute"
 import Signin from "./components/Signin"
 import Signup from "./components/Signup"
@@ -9,15 +11,17 @@ import { AuthContextProvider } from "./context/AuthContext"
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-center text-3xl font-bold">
         Firebase Auth & Context
       </h1>
       <AuthContextProvider>
         <Routes>
           <Route element={<ProtectedRoute />}>
+            <Route element={<Home />} path="/" />
             <Route element={<Account />} path="/account" />
           </Route>
-          <Route element={<Signin />} path="/" />
+          <Route element={<Signin />} path="/login" />
           <Route element={<Signup />} path="/register" />
         </Routes>
       </AuthContextProvider>
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="max-w-[600px] mx-auto my-16 p-4">
+      <h1 className="text-2xl font-bold py-2">Home</h1>
+    </div>
+  )
+}
+
+export default Home
